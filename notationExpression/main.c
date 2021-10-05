4fe666a1e0e877adc6a48f9af20ddc85fed68513
JaeyoonCheon <cjswodbs50@gmail.com>
2021-10-05 17:34:34 +0900

    infix to postfix convert postfix expression evaluation

--- notationExpression/main.c
+++ notationExpression/main.c
@@ -21,9 +21,14 @@ typedef enum precedence {
 	operand,
 } precedence;
 
-char stack[MAX_STACK_SIZE];
+int stack[MAX_STACK_SIZE];
 char expr[MAX_STR_SIZE];
+char postfixExpr[MAX_STR_SIZE];
 int top = -1;
+int first = 0;
+
+int isp[] = { 0, 19, 12, 12, 13, 13, 13, 0 };
+int icp[] = { 20, 19, 12, 12, 13, 13, 13, 0 };
 
 void push(char exp) {
 	if (top >= MAX_STACK_SIZE - 1) {
@@ -53,7 +58,7 @@ precedence getToken(char* symbol, int* n) {
 	case '*': return times;
 	case '/': return divide;
 	case '%': return mod;
-	case ' ': return eos;
+	case '\0': return eos;
 	default: return operand;
 	}
 }
@@ -99,6 +104,31 @@ int eval() {
 	return pop();
 }
 
+void printToken(precedence token)
+{/* print out the character equivalent of the token */
+	switch (token) {
+	case  plus:   
+		printf("+");
+		postfixExpr[first++] = '+';
+		break;
+	case  minus:  
+		printf("-");
+		postfixExpr[first++] = '-';
+		break;
+	case  divide: 
+		printf("/");
+		postfixExpr[first++] = '/';
+		break;
+	case  times:  
+		printf("*");
+		postfixExpr[first++] = '*';
+		break;
+	case  mod:    
+		printf("%");
+		postfixExpr[first++] = '%';
+	}
+}
+
 //infix -> postfix
 //1. 연산식 string char배열로 입력받음
 //2. string이 괄호가 포함된 식인 지 확인
@@ -109,18 +139,52 @@ int eval() {
 //7. 스택에 연산자가 top인데 연산자를 만나면 저장된 연산자를 pop하여 출력
 //8.)->stack top의(pop하여 제거
 
+void postfix() {
+	char symbol;
+	precedence token;
+	int n = 0;
+
+	top = 0;
+	stack[0] = eos;
+
+	for (token = getToken(&symbol, &n); token != eos; token = getToken(&symbol, &n)) {
+		if (token == operand) {
+			printf("%c", symbol);
+			postfixExpr[first++] = symbol;
+		}
+		else if (token == rparen) {
+			while (stack[top] != lparen) {
+				printToken(pop());
+			}
+			pop();
+		}
+		else {
+			while (isp[stack[top]] >= icp[token]) {
+				printToken(pop());
+			}
+			push(token);
+		}
+	}
+	while ((token = pop()) != eos) {
+		printToken(token);
+	}
+	postfixExpr[first++] = '\0';
+	printf("\n");
+}
+
 int main() {
 	int i;
-	char exp[MAX_EXP_SIZE];
 
-	createStack();
-
-	if (!scanf("%s", exp)) {
+	printf("input INFIX expression : ");
+	if (!scanf("%s", expr)) {
 		fprintf(stderr, "Can't read expresstion\n");
 		exit(1);
 	}
+	
+	printf("POSTFIX expression : ");
+	postfix();
 
-	do {
+	strcpy(expr, postfixExpr);
 
-	} while (top > -1);
+	printf("result : %d\n", eval());
 }
