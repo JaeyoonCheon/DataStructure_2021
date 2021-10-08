7db773a6bca1d6326aa521f983bc6d9345f26081
JaeyoonCheon <cjswodbs50@gmail.com>
2021-10-08 15:34:32 +0900

    single linked list sorting/unsorting insert

---
+++ singleLinkedList/main.c
@@ -0,0 +1,197 @@
+#pragma warning(disable:4996)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+#define MALLOC(p,s) \
+if ((p = malloc(s)) == NULL) {	\
+		fprintf(stderr, "Insufficient memory"); \
+		exit(EXIT_FAILURE); \
+}
+
+typedef struct listNode* listPointer;
+typedef struct listNode {
+	int data;
+	listPointer link;
+}listNode;
+
+void printList(listPointer* first) {
+	listPointer tail;
+	tail = (*first);
+
+	while (tail->link != NULL) {
+		printf("%d ", tail->data);
+		tail = tail->link;
+	}
+	printf("%d", tail->data);
+}
+
+void insert(listPointer* first, int _data) {
+	listPointer	temp;
+	listPointer pointer;
+
+	MALLOC(temp, sizeof(listNode));
+	//temp = (listPointer)malloc(sizeof(listNode));
+	temp->data = _data;
+	temp->link = NULL;
+
+	pointer = (*first);
+
+	if (pointer) {
+		//추가하는 데이터가 first의 data보다 작을 때 앞에 추가
+		if (pointer->data >= _data) {
+			temp->link = (*first);
+			(*first) = temp;
+		}
+		//이외의 값은 순서대로 조사해 가면서 다다음 값이 _data보다 큰 경우 다음 값 뒤에 추가
+		else {
+			while (pointer->link != NULL && pointer->link->data <= _data) {
+				pointer = pointer->link;
+			}
+
+			temp->link = pointer->link;
+			pointer->link = temp;
+		}
+	}
+	else {
+		//linked list에 아무 node가 없는 경우
+		(*first) = temp;
+	}
+}
+
+void insertFirst(listPointer* first, int _data) {
+	listPointer	temp;
+
+	MALLOC(temp, sizeof(listPointer));
+	//temp = (listPointer)malloc(sizeof(listPointer));
+
+	temp->data = _data;
+	temp->link = NULL;
+
+	if (*first) {
+		temp->link = (*first);
+		(*first) = temp;
+	}
+	else {
+		(*first) = temp;
+	}
+}
+
+void insertLast(listPointer* first, int _data) {
+	listPointer	temp;
+	listPointer tail;
+
+	MALLOC(temp, sizeof(listPointer));
+	//temp = (listPointer)malloc(sizeof(listPointer));
+	temp->data = _data;
+	temp->link = NULL;
+
+	tail = (*first);
+
+	if (*first) {
+		while (tail->link != NULL) {
+			tail = tail->link;
+		}
+
+		temp->link = tail->link;
+		tail->link = temp;
+	}
+	else {
+		(*first) = temp;
+	}
+}
+
+void sortedList() {
+	FILE* fp;
+	listPointer list = NULL;
+	int data;
+	int x;
+
+	fp = fopen("in.txt", "r");
+
+	while (!feof(fp)) {
+		fscanf(fp, "%d", &data);
+
+		insert(&list, data);
+	}
+
+	while (1) {
+		scanf("%d", &x);
+		insert(&list, x);
+		printList(&list);
+		printf("\n");
+	}
+}
+
+void unSortedList() {
+	FILE* fp;
+	listPointer list = NULL;
+	char dir[5];
+	int data;
+	int x;
+
+	fp = fopen("in.txt", "r");
+
+	printf("Input file data at head or tail? head/tail : ");
+	scanf("%s", dir);
+	printf("\n");
+	getchar();
+
+	while (!feof(fp)) {
+		fscanf(fp, "%d", &data);
+
+		if (!strcmp(dir, "head")) {
+			insertFirst(&list, data);
+		}
+		else if (!strcmp(dir, "tail")) {
+			insertLast(&list, data);
+		}
+		else {
+			return;
+		}
+	}
+
+	printf("Input data at head or tail? head/tail : ");
+	scanf("%s", dir);
+	getchar();
+
+	while (1) {
+		scanf("%d", &x);
+
+		if (!strcmp(dir, "head")) {
+			insertFirst(&list, x);
+		}
+		else if (!strcmp(dir, "tail")) {
+			insertLast(&list, x);
+		}
+		else {
+			return;
+		}
+
+		printList(&list);
+		printf("\n");
+	}
+}
+
+int main() {
+	char isSort;
+	int i;
+
+	while (1) {
+		printf("Sorted linked list? Y/N : ");
+		scanf("%c", &isSort);
+		printf("\n");
+
+		if (isSort == 'Y') {
+			sortedList();
+		}
+		else if (isSort == 'N') {
+			unSortedList();
+		}
+		else {
+			continue;
+		}
+	}
+}
